b7a367add631b67fd15e97d2e0fbd9ba3e0f925d
Leonardo Meneguetti <leonardo.meneguetti@aluno.ifsp.edu.br>
2024-03-11 19:07:14 -0300

    a

---
+++ DW1S5/Aula03.08/Exercicios/Exercicio2/script.js
@@ -0,0 +1,20 @@
+window.onload = iniciarPagina;
+
+function iniciarPagina() {
+    document.getElementById("form").onsubmit = function() {
+        return validarSenha();
+    }
+    document.getElementById("form").noValidate = true;
+}
+
+function validarSenha() {
+    let senha = document.getElementById("password");
+    let senhaConf = document.getElementById("passwordconf");
+
+    if(!senha.value == senhaConf.value){
+        alert("As senhas devem ser iguais.")
+        senhaConf.setCustomValidity("Vai se fuder.")
+        return false;
+    }
+    return true;
+}
