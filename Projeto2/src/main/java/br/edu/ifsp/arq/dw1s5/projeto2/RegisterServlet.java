9b9c9c5d113bc53cac4dc61c0f764a177db1549a
leonardomeneguetti <lrsmeneguetti@gmail.com>
2024-03-21 11:40:19 -0300

    aula 21/03

--- Projeto2/src/main/java/br/edu/ifsp/arq/dw1s5/projeto2/RegisterServlet.java
+++ Projeto2/src/main/java/br/edu/ifsp/arq/dw1s5/projeto2/RegisterServlet.java
@@ -50,6 +50,6 @@ public class RegisterServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		doGet(req, resp);
 	}
 }
